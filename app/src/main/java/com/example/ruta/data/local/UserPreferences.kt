f73b6aab2352d8383862de1c1d003849dee7e39a
karlaQuintanaR <s22120215@alumnos.itsur.edu.mx>
2026-04-08 12:47:48 -0600

    Para que funcione latitud y longitud

--- app/src/main/java/com/example/ruta/data/local/UserPreferences.kt
+++ app/src/main/java/com/example/ruta/data/local/UserPreferences.kt
@@ -7,21 +7,39 @@ import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
+
+// Se define fuera de la clase para asegurar que solo haya una instancia en toda la app (Singleton).
 val Context.dataStore by preferencesDataStore(name = "settings_pref")
 
 class UserPreferences(private val context: Context) {
+
+    // Objeto  para definir las llaves de acceso a los datos
     companion object {
+        // Definimos las llaves para Latitud y Longitud
         val HOME_LAT = doublePreferencesKey("home_lat")
         val HOME_LON = doublePreferencesKey("home_lon")
     }
 
+    /**
+     * Recupera la ubicación guardada como un flujo (Flow).
+     * Si la latitud es 0.0, asume que no hay datos y retorna null.
+     */
     val homeLocation: Flow<Pair<Double, Double>?> = context.dataStore.data.map { pref ->
         val lat = pref[HOME_LAT] ?: 0.0
         val lon = pref[HOME_LON] ?: 0.0
+
+        // Lógica de negocio: Si lat es 0.0, devolvemos null (ubicación no configurada)
         if (lat != 0.0) Pair(lat, lon) else null
     }
 
+    /**
+     * Función suspenso para guardar las coordenadas de forma asíncrona.
+     * Debe ser llamada dentro de una Corrutina.
+     */
     suspend fun saveHomeLocation(lat: Double, lon: Double) {
-        context.dataStore.edit { it[HOME_LAT] = lat; it[HOME_LON] = lon }
+        context.dataStore.edit { preferences ->
+            preferences[HOME_LAT] = lat
+            preferences[HOME_LON] = lon
+        }
     }
 }
